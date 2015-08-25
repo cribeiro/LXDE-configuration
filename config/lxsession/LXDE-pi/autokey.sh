956b6f56e83fc0dfdbac611565931ceb78223c7c
spl <simon@raspberrypi.org>
2015-08-25 15:02:22 +0100

    Mods to enable auto keyboard selection to co-exist with lxkeymap.

--- config/lxsession/LXDE-pi/autokey.sh
+++ config/lxsession/LXDE-pi/autokey.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
-
-if lsusb -d 1c4f:0027; then
+if [ -e /home/pi/.config/lxkeymap.cfg ]; then
+  echo "lxkeymap found"
+  lxkeymap --autostart
+else
+  if lsusb -d 1c4f:0027; then
 	echo "Pi Keyboard"
 	setxkbmap us
-fi
-if lsusb -d 1c4f:0016; then
+  fi
+  if lsusb -d 1c4f:0016; then
 	echo "Black keyboard"
 	setxkbmap gb
+  fi
 fi
